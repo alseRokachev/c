bf825ea93bb2b72b0cb0f5ad5798a5bde1887389
Alex Rokachev <alserokachev@gmail.com>
2023-01-12 16:38:31 +0300

    added greeting

---
+++ examples/example005_greeting/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "alex") 
+{
+    Console.WriteLine("Дарова, Кент");
+} 
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
